2eb1579a580c0d49416b4499dcb543e77ce51fee
gautamz07 <gautamz07@gmail.com>
2019-07-08 16:03:32 +0530

    commiting latest changes
    commiting latest changes

--- test/autoplay.test.js
+++ test/autoplay.test.js
@@ -9,20 +9,48 @@ describe('check autoplay option' , () => {
         document.body.innerHTML = html;
     });
 
-    test( 'change active slide' , (done) => {
+    test( 'Check index to be 1 for autoplay' , (done) => {
 
-           let glide = new Glide('.glide' , {
-               autoplay: 500
-           });
+           let glide = new Glide('#glide' , {
+                type : 'slider',
+                gap : 0,
+                autoplay: 500,
+                rewind: false
+           }).mount();
 
            setTimeout( () => {
-                expect( Array.from( document.querySelectorAll('.glide__slide') )[1].contains('glide__slide') )
-                    .toBe(true);
+                console.log( `The glide index is ${glide.index}` );
+                expect( glide.index ).toBe(1);
                 done();
-
            }, 510 );
 
+        //    setTimeout( () => {
+        //      console.log( `The glide index is ${glide.index}` );
+        //      expect( document.querySelectorAll('.glide__slide')[2].classList.contains('glide__slide--active') )
+        //         .toBe(true);
+        //      done();
+        //    }, 1010 );
+
     });
+
+    /*
+    test( 'Check index to be 2 for autoplay' , (done) => {
+
+            let glide = new Glide('#glide' , {
+                type : 'slider',
+                gap : 0,
+                autoplay: 500,
+                rewind: false
+            }).mount();
+
+            setTimeout( () => {
+                console.log( `The glide index is ${glide.index}` );
+                expect( glide.index ).toBe(2);
+                done();
+            }, 1010 );
+
+    });
+    */
 
 });
 
